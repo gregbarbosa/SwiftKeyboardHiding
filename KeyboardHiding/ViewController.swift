88124554b2d07244459ed8422220c6e28399627b
Greg Barbosa <gregb@gregbarbosa.local>
2014-08-11 13:12:54 -0400

    Initial Commit

---
+++ KeyboardHiding/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KeyboardHiding
+//
+//  Created by Greg Barbosa on 8/11/14.
+//  Copyright (c) 2014 gregbarbosa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
